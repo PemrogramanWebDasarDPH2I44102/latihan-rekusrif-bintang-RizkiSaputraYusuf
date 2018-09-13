663ec75b0b2744b06dffc1449c5fec22e9c0f26b
RizkiSaputraYusuf <42662979+RizkiSaputraYusuf@users.noreply.github.com>
2018-09-13 09:03:37 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,34 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+echo"<b>Bintang Piramida dengan menggunakan fungsi Rekursif</b><br><br>";
+function rekursif ($batas){
+	echo "<pre>";
+	for ($i=1;$i<=$batas;$i++){
+     for ($j=$batas;$j>=$i;$j--){
+    echo "*";
+      }
+     echo "<br>";
+}
+	echo "</pre>";
 }
 rekursif(5);
+echo"=================================";
+
+echo"<b>Piramida Angka dengan menggunakan fungsi Rekursif</b><br><br>";
+function rekursif2 ($batas2){
+	echo "<pre>";
+	for ($i=1; $i <=$batas2 ; $i++) {
+ for ($j=4; $j>=$i ; $j--) {
+ echo "&nbsp";
+ }
+for ($k=$i; $k >=1 ; $k--) {
+ echo "$k";
+ }
+ echo "<br>";
+}
+echo"<pre>";
+ 
+}
+
+rekursif2(5);
+
 ?>
